2ce71b6048d300ab5b48c6d2295ddbb5403df363
DESKTOP-S2C7CJG\computer <kys@greenit.cc>
2023-01-05 08:53:49 +0900

    first commit

---
+++ src/main/java/com/example/projectSang/ProjectSangApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectSang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSangApplication.class, args);
+	}
+
+}
